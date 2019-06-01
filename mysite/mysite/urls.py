bf335c33c89080d90321850712dcb7dd4cf15aa9
Marceline7 <lost.alax@yandex.ru>
2019-06-01 10:45:26 +0300

    1

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from articles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include ('firstwebpage.urls'))
+    path('',include ('firstwebpage.urls')),
+    path('articles/', views.archive, name='archive')
 ]
